76748311d27d986a3b165b7903c731aa44f647ae
Vamjadeep1103 <165111198+Vamjadeep1103@users.noreply.github.com>
2025-01-27 17:05:47 +0530

    Update stack-heap.js

--- 01_basic/stack-heap.js
+++ 01_basic/stack-heap.js
@@ -1,1 +1,23 @@
+// stack(primitive)  heap(Non-Premitive)
+
+let myclg = "SSASIT"
+
+let anotherclg = "ppsv"
+anotherclg = myclg
+
+console.log(myclg)
+console.log(anotherclg)
+
+let user1 ={
+    email:"acv@gmail.com",
+    id:1234
+
+}
+
+let user2 = user1
+
+user2.email = "hfgh@gmail"
+console.log(user1.email)
+console.log(user2.email)
+
 
