94a887be5abab30f5044b4b19d18d844b0b4bcae
Vamjadeep1103 <165111198+Vamjadeep1103@users.noreply.github.com>
2025-01-27 10:49:07 +0000

    summary

---
+++ 01_basic/summary.js
@@ -0,0 +1,24 @@
+// primitive 
+
+// 7 types : String,number,boolen,null,,undefined,symbol,BigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false;
+let userEmail;
+
+
+const id = Symbol('123')
+const Anid = Symbol('123')
+
+console.log(id == Anid)
+
+const bigNumber = 23564125634125n
+console.log(bigNumber)
+
+//Non primitive
+
+//arays,objects,function
+
+
