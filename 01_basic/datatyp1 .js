27d4303c521037b40b1507f7e2c5d99b407ee53a
Vamjadeep1103 <165111198+Vamjadeep1103@users.noreply.github.com>
2025-01-27 07:19:05 +0000

    str-num

---
+++ 01_basic/datatyp1 .js
@@ -0,0 +1,20 @@
+"use strict"
+ 
+let name = "Deep"   //string data type
+let age = 18    //number data type
+let isLoggedIn = false //boolean data type
+let state = null
+
+
+//bigint D.T.
+//String =>
+//bollean D.T.(true,false)
+//null => satnd alone value
+// undefined =>
+// symbol(unquienest find )
+
+//object
+
+console.log(typeof "Deep")
+console.log(typeof age)
+console.log(typeof null)  //here null data type is object
