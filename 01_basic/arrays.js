3b67c0516a5dc5c29e5694e3210776d0672592a3
Vamjadeep1103 <165111198+Vamjadeep1103@users.noreply.github.com>
2025-01-28 07:19:22 +0000

    arrays

---
+++ 01_basic/arrays.js
@@ -0,0 +1,36 @@
+// arrays
+
+//const myarr =[0,1,2,3]
+//console.log(myarr)
+
+//const myname =["deep",'jay',"amar"]  name will be written [""]  
+
+//const myarr2 =new Array(1,2,3,4)
+//console.log(myarr2)
+
+// Arrays method
+
+//myarr.push(6)
+//myarr.pop(6)
+//myarr.unshift(9)
+//myarr.shift()
+//console.log(myarr.includes(7))
+//console.log(myarr.indexOf(3))
+//console.log(myarr)
+
+
+//const newarr = myarr.join()     //join will be convert array to String datatypes
+
+//console.log(myarr)
+//console.log(typeof newarr)
+
+// slice and splice
+
+//const a = ["b","c","d","e"]
+//console.log(a.slice(2,3))
+
+
+let months=['Jan', 'March', 'April', 'June'] //Starting from index 1 (which is the second element ‘March’).
+months.splice(2,'feb'); //Remove 1 element (which is ‘March’).
+
+console.log(months)//add feb in palce of that
