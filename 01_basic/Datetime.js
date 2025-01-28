88e6015148ea0f892b5d0fd6bb22ead8fac57124
Vamjadeep1103 <165111198+Vamjadeep1103@users.noreply.github.com>
2025-01-28 05:47:43 +0000

    datetime

---
+++ 01_basic/Datetime.js
@@ -0,0 +1,11 @@
+//dates
+
+//let Date1 = new Date()
+//console.log(Date1.toString())
+//console.log(Date1.toDateString())
+//console.log(Date1.toLocaleString())
+//console.log(typeof myDate())
+
+
+let mycreatedDate = new Date(2023,0,23)
+let mycreatedDate1 = new Date(2023,0,23,5,3)
