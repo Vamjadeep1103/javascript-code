7b788deed84889a082d9600dcd76025f38defa92
Vamjadeep1103 <165111198+Vamjadeep1103@users.noreply.github.com>
2025-01-30 10:33:09 +0000

    nullish operator

--- 01_basic/0018_controlflow.js
+++ 01_basic/0018_controlflow.js
@@ -1,9 +1,9 @@
-let month = 1       //these datatype is number string also same method //
-switch(month){
+//let month = 1       //these datatype is number string also same method //
+/* switch(month){
         case 1:
             console.log("jan")
             break;
-        case 2:
+        //case 2:
             console.log("feb")
             break;
         case 3:
@@ -12,8 +12,8 @@ switch(month){
         default:
             console.log("no match")
             break;
-
-}
+*/
+//}
 
 //-------------false values---------//
 
@@ -24,15 +24,22 @@ switch(month){
 // "0",'false'," ",[],{},function(){}
 
 
-const useremail = ""
-if (useremail.length === 0){        //here array is empty//
-    console.log("Array is empty")
-}
+//const useremail = ""
+//if (useremail.length === 0){        //here array is empty//
+    //console.log("Array is empty")
+//}
 
-const emptyobj = {}
+//const emptyobj = {}
 
-if(Object.keys(emptyobj).length === 0){ //here object is empty//
-    console.log("object is empty")
-}
+//if(Object.keys(emptyobj).length === 0){ //here object is empty//
+   //console.log("object is empty")
+//}
 
 //Nulish Coalescing Operator (??)
+
+//let val1
+//val1 = 5 ?? 10
+//val1 = null ?? 10 //expeced o/p will be null
+//val1 = undefined ?? 15  //expected o/p will be 15 because undefined is not defined data type
+val1 = null ?? 10 ?? 20
+console.log(val1)
