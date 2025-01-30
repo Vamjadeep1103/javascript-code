c33b70554d2fc05f0ddb16cce9a0fe2032a32db2
Vamjadeep1103 <165111198+Vamjadeep1103@users.noreply.github.com>
2025-01-30 05:44:47 +0000

    IIfe

---
+++ 01_basic/016_IIfe.js
@@ -0,0 +1,20 @@
+//imediate invoked function //
+
+let a = new Promise((resolve,reject)=>{     
+setTimeout(() => {
+   resolve(456) 
+}, 4000);
+})
+
+let f = async () => {
+    
+    let b = await a
+    console.log(b)
+    let c = await a
+    console.log(c)
+    let d = await a
+    console.log(d)
+
+}
+
+f()
