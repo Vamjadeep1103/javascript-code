124e8c8c3197975c170e3a2baccc8037b233a806
Vamjadeep1103 <165111198+Vamjadeep1103@users.noreply.github.com>
2025-01-31 05:03:42 +0000

    loops in js

--- 01_basic/019_loop.js
+++ 01_basic/019_loop.js
@@ -7,16 +7,36 @@
 }
 */
 
-function search(arr, value) {
-    for (var i = 0; i < arr.length; i++) {
-       if (arr[i] === value) {
-          return i;
-       }
-    }
-    return -1;
- }
+// function search(arr, value) {
+//     for (var i = 0; i < arr.length; i++) {
+//        if (arr[i] === value) {
+//           return i;
+//        }
+//     }
+//     return -1;
+//  }
  
- var nums = [1, 2, 3];
- console.log(search(nums, 1));
- console.log(search(nums, 3));
- console.log(search(nums, 30));
+//  var nums = [1, 2, 3];
+//  console.log(search(nums, 1));
+//  console.log(search(nums, 3));
+//  console.log(search(nums, 30));
+
+ //break and continue statement in loops//
+
+//  for(let index=1;index<=20;index++){
+//    if(index == 5){
+//       console.log(`detected 5`)
+//       break
+//    }
+//    console.log(`value of i is ${index}`);
+
+//  }
+
+for(let index=1;index<=20;index++){
+   if(index == 5){
+      console.log(`detected 5`)
+      continue
+   }
+   console.log(`value of i is ${index}`);
+
+ }
