362e17b6a48292ce3a16814588c0bbb4e90cb79b
Vamjadeep1103 <165111198+Vamjadeep1103@users.noreply.github.com>
2025-01-31 08:42:22 +0000

    do while loop in js

---
+++ 01_basic/020_while-dowhile.js
@@ -0,0 +1,26 @@
+//normal while program //
+
+// let number
+// while (number < 10 ){
+//         console.log(`value of index is ${number}`)
+//         number = number + 2
+// }
+
+// //in loops arrays defined exampple//
+
+// let myarray = ["deep", "jay","raj"]
+// let arr = 0
+// while(arr<myarray.length){
+//         console.log(`value is ${myarray[arr]}`)
+//         arr = arr + 1
+// }
+
+// do while loops //
+
+// special case in do while was first check value and then condition check //
+let score = 1
+do {
+        console.log(`score is ${score}`)
+        score++
+} while (score <= 10)
+
