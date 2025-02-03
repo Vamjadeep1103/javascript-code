b50bd432c071d24f9f3bbe886e1fb5b56d2343f9
Vamjadeep1103 <165111198+Vamjadeep1103@users.noreply.github.com>
2025-02-03 05:37:07 +0000

    filter,map and reduce element in js

--- 01_basic/024_filtermap.js
+++ 01_basic/024_filtermap.js
@@ -1,4 +1,5 @@
 //filter has uses many times in js //
+
 //filter has used with arrow function //
 
 // const mynum =[1,2,3,4,5,6,7,8,9,10]
@@ -22,19 +23,49 @@ let books =[
 ]
 //let userbooks = books.filter ((bk) => bk.Genre === "Fiction")
 let userbooks1 = books.filter((bk) => {return bk.Publish >= 1950})
-console.log(userbooks1);
+//console.log(userbooks1);
 
 
-// map() using in javascript //
+
+
+// ------map() using in javascript------ //
 
 const mynumber = [1,2,3,4,5,6,7,8,9,10]
-
 //const newnumber = mynumber.map((num) =>{return num + 10})
 const newnumber  = mynumber.map((num) => num * 10)
                             .map((num) => num+1)
                             .filter((num) => num > 40)
                             .reverse()
-console.log(newnumber);
+ console.log(newnumber);
 
 
 
+
+// -------reduce() function use in js ------//
+const myNums  = [1,2,3]
+ const myTotal = myNums.reduce(function(accumulator,currentvalue){
+    console.log(`accumulator: ${accumulator} and currentvalue :${currentvalue}`)
+     return accumulator  + currentvalue
+ },0)
+ console.log(myTotal);
+
+//------reduce() function with arrow function example----//
+
+const myTotal1 = myNums.reduce((acc,curr) => acc+curr,0)
+console.log(myTotal1);
+
+
+const shoppingcart = [
+    {
+        itemname: "js",
+        price: 1000
+    },{
+        itemname: "html",
+        price: 1234
+    },{
+        itemname: "js",
+        price: 1898
+    }
+]
+const totalpay = shoppingcart.reduce((acc,itemname)=> acc+itemname.price,0)
+console.log(totalpay)
