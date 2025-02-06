b5ee6738c210ec6dded5eaf7b39fda7e7625ed4a
Vamjadeep1103 <165111198+Vamjadeep1103@users.noreply.github.com>
2025-02-06 09:10:23 +0000

    event in js

---
+++ 01_basic/028_event.js
@@ -0,0 +1,16 @@
+//type,timestap,defaultPrevented
+//target,toElement,srcelement,currentTarget
+//clientX,clientY,screenX,screenY
+//altkey,ctrlkey,shiftkey,keycode
+
+
+
+/* this is event on click images when images is removed */
+/*document.queryselector('#images').addEventListener('click',function(e){
+console.log(e.target.tagName);
+console.log(e.target.tagName === 'IMG'){
+console.log(e.target.id);
+let removeIt = e.target.parentNode
+removeIt.remove()
+}
+})*/
